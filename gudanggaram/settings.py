a1580856eb4cda98e6a78c3d0e2d978539f398b5
Alden Luthfi <alden.luthfi@ui.ac.id>
2023-12-03 11:20:10 +0700

    Updated

--- gudanggaram/settings.py
+++ gudanggaram/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://alden-luthfi-tugas.pbpcs.ui.ac.id']
+CSRF_TRUSTED_ORIGINS = ['https://alden-luthfi-tugas.pbp.cs.ui.ac.id']
 
 
 # Application definition
